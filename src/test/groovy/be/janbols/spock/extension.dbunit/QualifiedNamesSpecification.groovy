43f6d1574ede4eb59e202fa257453a30b4b7c111
Oscar Korz <okorz@yahoo-inc.com>
2017-07-17 13:47:25 -0700

    Add test for qualified table names

---
+++ src/test/groovy/be/janbols/spock/extension.dbunit/QualifiedNamesSpecification.groovy
@@ -0,0 +1,44 @@
+package be.janbols.spock.extension.dbunit
+
+import groovy.sql.Sql
+import org.apache.tomcat.jdbc.pool.DataSource
+import org.dbunit.database.DatabaseConfig
+import spock.lang.Specification
+
+import static SpecUtils.inMemoryDataSource
+
+/**
+ * Specification showing how to use qualified table names
+ */
+class QualifiedNamesSpecification extends Specification {
+
+    DataSource dataSource
+
+    @DbUnit(configure = {
+        it.connection.config.setProperty(DatabaseConfig.FEATURE_QUALIFIED_TABLE_NAMES, true)
+    })
+    def content = {
+        //Foo.Bar(id: 42)
+        'Foo.Bar'(id: 42)
+    }
+
+    def setup() {
+        dataSource = inMemoryDataSource()
+        def sql = new Sql(dataSource)
+        sql.execute("CREATE SCHEMA if not exists Foo")
+        sql.execute("CREATE TABLE Foo.Bar(id INT PRIMARY KEY)")
+    }
+
+    def cleanup() {
+        new Sql(dataSource).execute("drop table Foo.Bar")
+        new Sql(dataSource).execute("drop schema Foo")
+    }
+
+    def "dbUnit fills the table in the correct schema when using qualified names"() {
+        when:
+        def foobar = new Sql(dataSource).firstRow("select * from Foo.Bar")
+
+        then:
+        foobar.id == 42
+    }
+}
